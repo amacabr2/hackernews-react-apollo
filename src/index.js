8d820810555fe47d0c012afd608c340d5bf5c23d
amacabr2 <anthony.macabrey70@gmail.com>
2019-05-03 21:39:54 +0200

    Requiring authentication on the server-side

--- src/index.js
+++ src/index.js
@@ -11,15 +11,10 @@ import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context'
 
-import { AUTH_TOKEN } from '../constants'
+import { AUTH_TOKEN } from './constants'
 
 const httpLink = createHttpLink({
 	uri:'http://localhost:4000'
-})
-
-const client = new ApolloClient({
-	link: authLink.concat(httpLink),
-	cache: new InMemoryCache()
 })
 
 const authLink = setContext((_, { headers }) => {
@@ -30,6 +25,11 @@ const authLink = setContext((_, { headers }) => {
       authorization: token ? `Bearer ${token}` : ''
     }
   }
+})
+
+const client = new ApolloClient({
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache()
 })
 
 ReactDOM.render(
